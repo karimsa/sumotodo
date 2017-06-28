b934885e21c68864b7a6e39f467dfdcf49a39846
Karim Alibhai <karim@alibhai.co>
2017-06-28 00:22:29 -0400

    Add revamped server
    * Add lock & interval based data saving (to support persistent storage at an efficient rate)
    * Handle post requests from page for no-JS browsers
    * Render initial page from server instead of waiting for socket.io to load
    * Switch http server to statically run entire website, not just socket.io
    * Use env.PORT instead of preset port

--- index.js
+++ index.js
@@ -1,39 +1,146 @@
-const server = require('socket.io')();
-const firstTodos = require('./lib/data');
-const Todo = require('./lib/todo');
+/**
+ * @file index.js
+ * @description Main io logic.
+ */
 
-server.on('connection', (client) => {
-    // This is going to be our fake 'database' for this application
-    // Parse all default Todo's from db
+// config defaults
+process.env.PORT = process.env.PORT || 8080
 
-    // FIXME: DB is reloading on client refresh. It should be persistent on new client
-    // connections from the last time the server was run...
-    const DB = firstTodos.map((t) => {
-        // Form new Todo objects
-        return new Todo(title=t.title);
-    });
+const fs = require('fs')
+    , path = require('path')
+    , util = require('util')
+    , express = require('express')
+    , Todo = require('./lib/todo')
+    , app = express()
 
-    // Sends a message to the client to reload all todos
-    const reloadTodos = () => {
-        server.emit('load', DB);
+    // since this is only for demo purposes, just going
+    // to use the regular http (no ssl)
+    , http = require('http').createServer(app)
+
+    // bind socket.io to the same http instance
+    , io = require('socket.io')(http)
+
+// load currrent data
+let db = require('./lib/data').map(t => new Todo(t))
+
+// lockdown - similar to debounce but doesn't wait
+// for last call
+let storeDataLock = false
+let storeDataAgain = false
+
+// store data cycle
+function storeData( force = false ) {
+  if (storeDataLock && !force) return (storeDataAgain = true)
+
+  /**
+   * Discard done items.
+   */
+  db = db.filter(t => !t.done)
+
+  /**
+   * Write to disk. This is just one way to implement the persistent
+   * real-time storage when using files (that's not scalable, but that's
+   * within the constraints of the challenge).
+   */
+  fs.writeFile('./lib/data.json', JSON.stringify(db), err => {
+    console.log('  db updated')
+
+    if (err) console.error(err && err.stack ? err.stack : err)
+    
+    // forced restore... in a bit
+    if (storeDataAgain) {
+      return setTimeout(() => storeData(true), 100)
     }
 
-    // Accepts when a client makes a new todo
-    client.on('make', (t) => {
-        // Make a new todo
-        const newTodo = new Todo(title=t.title);
+    // release the lock
+    storeDataLock = false
+  })
+}
 
-        // Push this newly created todo to our database
-        DB.push(newTodo);
+/**
+ * Handle the data storage.
+ */
+app.post('/', require('body-parser').urlencoded({ extended: true }), (req, res, next) => {
+  const todo = new Todo({
+    text: req.body.todoText,
+    done: false
+  })
 
-        // Send the latest todos to the client
-        // FIXME: This sends all todos every time, could this be more efficient?
-        reloadTodos();
-    });
+  // update the db
+  console.log('  add %j', todo)
+  db.push(todo)
+  storeData()
 
-    // Send the DB downstream on connect
-    reloadTodos();
-});
+  // move to regular routing
+  next()
+})
 
-console.log('Waiting for clients to connect');
-server.listen(3003);
+/**
+ * Static-ish. A bit of server-side rendering.
+ */
+const static = express.static(`${__dirname}/www`)
+app.use((req, res, next) => {
+  const file = '.' + (req.url === '/' ? '/index.html' : req.url)
+  const pathtofile = path.resolve(__dirname, 'www' , file)
+
+  /**
+   * If index, do some custom rendering.
+   */
+  if (file === './index.html') {
+    const html = fs.readFileSync(pathtofile, 'utf8')
+    return res.end(util.format.call(util, html, db.join('')))
+  }
+
+  /**
+   * Send file.
+   */
+  res.sendFile(pathtofile)
+})
+
+/**
+ * Handle real-time communication.
+ */
+io.on('connection', client => {
+  client.emit('load', db)
+
+  client.on('add', t => {
+    try {
+      // validate
+      t = new Todo(t)
+
+      // push this newly created todo to our database
+      db.push(t)
+
+      // resave
+      console.log('  add %j', t)
+      storeData()
+    } catch (err) {
+      console.error('  fail Invalid todo: %s', err && err.stack ? err.stack : err)
+      client.emit('fail', `Invalid todo item: "${t}"`)
+    }
+  })
+
+  /**
+   * Updating of state. This really just discards the todo.
+   */
+  client.on('update', todo => {
+    for (let t of db) {
+      if (t.text === todo.text) {
+        t.done = todo.done
+        console.warn('  toggle %j', todo)
+        return storeData()
+      }
+    }
+  })
+})
+
+// prepare for failures
+http.on('error', err => {
+  console.error('Failed to start on port: %s', process.env.PORT)
+  console.error('%s', err && err.stack ? err.stack : err)
+})
+
+// startup io
+http.listen(process.env.PORT, () => {
+  console.log('Listening @ :%s', process.env.PORT)
+})
